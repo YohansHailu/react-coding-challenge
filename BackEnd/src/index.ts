9d133dd97b01b385365d8fab918287c064c444a5
yohans hailu <emailyyohans@gmail.com>
2023-12-31 14:08:54 +0300

    index.ts set up

---
+++ BackEnd/src/index.ts
@@ -0,0 +1,36 @@
+import express, { Express, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors'
+import process from 'process';
+
+dotenv.config();
+
+
+const port = process.env.PORT || 5000;
+const mongodbUrl = process.env.DATABASE_URL || "";
+const app: Express = express();
+
+
+mongoose.connect(mongodbUrl).
+  catch(error =>
+    console.log(error)
+  );
+mongoose.connection.on('error', err => {
+  console.log(err);
+});
+
+
+
+app.use(express.json());
+app.use(cors())
+
+
+app.get("/helloworld", (_, res: Response) => {
+  res.send('helloworld')
+})
+
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
